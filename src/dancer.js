a660d8b0f47c859eaf24695d32c3c5cb87f17583
tivrama <tivrama@yahoo.com>
2016-05-09 15:03:31 -0700

    still working on settimeout

--- src/dancer.js
+++ src/dancer.js
@@ -1,20 +1,30 @@
-// Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
 
-  var dancer = {};
+var MakeDancer = function(top, left, timeBetweenSteps){
+
+  this.$node = $('<span class="dancer"></span>');
+  this.step();
+  this.setPosition(top, left);
+  //this.stepTime = timeBetweenSteps; experiment with set timeout
+};
+
+
+  // var dancer = {}; dont need this
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
 
 
-  dancer.step = function(){
+  MakeDancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+    var context = this;
+    // var callMethod = function() {
+    //   context.step();
+    // }
+    window.setTimeout(context.step, context.timeBetweenSteps);
+    //this.step;
   };
-  dancer.step();
 
-  dancer.setPosition = function(top, left){
+  MakeDancer.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
@@ -22,12 +32,10 @@ var makeDancer = function(top, left, timeBetweenSteps){
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
 
-  return dancer;
-};
+  // return dancer;
